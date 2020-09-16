35aea0b58b248ee5a4e17255fa71d3c0d5f28059
Peter <56226439+piotrax@users.noreply.github.com>
2020-09-16 21:30:34 +0200

    Update app.js

---
+++ 2-petle/1-zadania/js/app.js
@@ -0,0 +1,11 @@
+// Zadanie 1
+// Za pomocą pętli wypisz w konsoli tekst 12345678910
+const tab1 = [];
+for (let i = 1; i < 11; i++){
+  tab1[i] = i;
+}
+console.log(tab1.join(""));
+
+//Zadanie 2
+// Stwórz zmienną n, pod którą postawisz jakąś liczbę całkowitą z przedziału 1-10 oraz dodatkową zmienną txt, która będzie zawierać jakiś tekst np. kot.
+// Sprawdź czy liczba n jest dodatnia. Jeżeli jest wypisz w konsoli tekst, który będzie powtórzeniem n razy tekstu ze zmiennej txt.
