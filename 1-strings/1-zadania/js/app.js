e165cb2f52f7e2660cfc849f5ff18c1c55e60fc3
Peter <56226439+piotrax@users.noreply.github.com>
2020-09-16 19:04:02 +0200

    Update app.js

--- 1-strings/1-zadania/js/app.js
+++ 1-strings/1-zadania/js/app.js
@@ -1,3 +1,5 @@
+// Zadanie 1 Mamy tekst: Koty to fajne zwierzaki Wypisz w konsoli liczbę wyrazów w tym zdaniu.
+
 let tekst = "Koty to fajne zwierzaki";
 
 function display(){
@@ -8,6 +10,22 @@ function display(){
     }
   }
   console.log(`Ilość wyrazów w stringu wynosi: ${x}.`);
-  document.getElementById("info").textContent =
-    "Ilość wyrazów w stringu wynosi: " + x +".";
+  //document.getElementById("info").textContent =
+  //  "Ilość wyrazów w stringu wynosi: " + x +".";
+  
+  
 }
+
+// Zadanie 2: Za pomocą funkcji Prompt() pobierz od użytkownika jego imię.
+//            Następnie używając odpowiednich funkcji dla stringów wypisz 
+//            w konsoli to imię tak by było pisane małymi,
+//            literami a rozpoczynało się od dużej litery
+
+const imie = prompt("podaj swoje imię: ").toLowerCase();
+const tab = [...imie];
+tab[0] = tab[0].toUpperCase();
+let name = ""
+for(let i in tab){
+  name +=(tab[i]);
+}
+console.log(`Witaj ${name}`);
