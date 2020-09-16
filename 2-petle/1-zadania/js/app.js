3433d51c9863f2955548a4f7cbdfa0838d974a16
Peter <56226439+piotrax@users.noreply.github.com>
2020-09-16 21:37:13 +0200

    Update app.js

--- 2-petle/1-zadania/js/app.js
+++ 2-petle/1-zadania/js/app.js
@@ -15,3 +15,17 @@ console.log(tab1.join(""));
 //Zadanie 2
 // Stwórz zmienną n, pod którą postawisz jakąś liczbę całkowitą z przedziału 1-10 oraz dodatkową zmienną txt, która będzie zawierać jakiś tekst np. kot.
 // Sprawdź czy liczba n jest dodatnia. Jeżeli jest wypisz w konsoli tekst, który będzie powtórzeniem n razy tekstu ze zmiennej txt.
+let n = 7;
+let txt = "mysz";
+if(n > 0){
+  for(let i = 0; i < n; i++){
+    console.log(txt);
+  }
+}
+
+// Zadanie 3
+/* Wygeneruj i wypisz w konsoli tekst:
+╔═══════════════════════╗
+║  To jest jakiś tekst  ║
+╚═══════════════════════╝
+Tekst w ramce powinien być postawiony pod zmienną, tak by łatwo można było go zmienić. Narożniki wygeneruj po prostu kopiując odpowiedni znaki z powyższego przykładu. */
