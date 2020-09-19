bf86e1bacdce1eb37deb9b2a94501c93e6b62235
Peter <56226439+piotrax@users.noreply.github.com>
2020-09-19 20:35:35 +0200

    Update app.js

--- 3-funkcje/1-zadania/js/app.js
+++ 3-funkcje/1-zadania/js/app.js
@@ -3,3 +3,7 @@
 Funkcja niech ZWRACA tekst Liczba liter: .... gdzie .... to liczba liter tekstu.
 Wynik jej użycia wypisz w konsoli za pomocą console.info
 */
+function textInfo(txt){
+  console.info(txt.length);
+}
+textInfo("To jest tekst do sprawdzenia czy funkcja policzy ilość liter oraz spacji. Powinno być ich razem 100.");
