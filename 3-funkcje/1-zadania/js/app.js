652fa5b0dbcd3f74dffe004b5dee6d1ec3921485
Peter <56226439+piotrax@users.noreply.github.com>
2020-09-19 20:18:41 +0200

    Update app.js

---
+++ 3-funkcje/1-zadania/js/app.js
@@ -0,0 +1,5 @@
+// Zadanie 1
+/* Napisz funkcję, która przyjmie jeden parametr - dowolny tekst.
+Funkcja niech ZWRACA tekst Liczba liter: .... gdzie .... to liczba liter tekstu.
+Wynik jej użycia wypisz w konsoli za pomocą console.info
+*/
