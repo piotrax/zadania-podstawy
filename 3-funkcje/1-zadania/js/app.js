0328abd6f04600323cfa0042811cd7e98796c266
Peter <56226439+piotrax@users.noreply.github.com>
2020-09-19 20:43:43 +0200

    Update app.js

--- 3-funkcje/1-zadania/js/app.js
+++ 3-funkcje/1-zadania/js/app.js
@@ -12,3 +12,15 @@ textInfo("To jest tekst do sprawdzenia czy funkcja policzy ilość liter oraz sp
 /* Napisz funkcję, która zsumuje przekazaną do niej tablicę i zwraca jej sumę.
    Stwórz dowolną tablicę, a następnie przekaż ją do tej funkcji i wynik wypisz w konsoli.
 */
+function sumArray(arr){
+	let sum = 0;
+	for(let i in arr){
+		sum += arr[i]
+	}
+	console.info(`Suma tablicy wynosi ${sum}.`);
+}
+myArr = [12, 98, 55, 28, 76, 52, 8, 43];
+sumArray(myArr);
+
+// Zadanie 3
+// Napisz funkcję, która przyjmie dowolny tekst. Funkcja niech zwraca tekst, który ma zmiksowana wielkość liter np:
