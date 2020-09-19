2ac94475de15c775e08a9a8caed636d3bf289d09
Peter <56226439+piotrax@users.noreply.github.com>
2020-09-19 19:34:28 +0200

    Update app.js

--- 2-petle/1-zadania/js/app.js
+++ 2-petle/1-zadania/js/app.js
@@ -77,3 +77,10 @@ console.log(thirdLine);
    01-2--3---4----5-----6------7-------
    Zadanie wykonaj dla zmiennej równej n=7
 */
+let str = "";
+let line = "-------";
+for(let n = 0; n <= 7; n++){
+	str += n;
+	str += line.slice(0, n);
+}
+console.log(str);
