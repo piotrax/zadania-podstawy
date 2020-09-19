3a1a2425dbf6ffef60e31243e0abd2db381d7d9d
Peter <56226439+piotrax@users.noreply.github.com>
2020-09-19 20:36:54 +0200

    Update app.js

--- 3-funkcje/1-zadania/js/app.js
+++ 3-funkcje/1-zadania/js/app.js
@@ -1,9 +1,14 @@
 // Zadanie 1
 /* Napisz funkcję, która przyjmie jeden parametr - dowolny tekst.
-Funkcja niech ZWRACA tekst Liczba liter: .... gdzie .... to liczba liter tekstu.
-Wynik jej użycia wypisz w konsoli za pomocą console.info
+   Funkcja niech ZWRACA tekst Liczba liter: .... gdzie .... to liczba liter tekstu.
+   Wynik jej użycia wypisz w konsoli za pomocą console.info
 */
 function textInfo(txt){
   console.info(txt.length);
 }
 textInfo("To jest tekst do sprawdzenia czy funkcja policzy ilość liter oraz spacji. Powinno być ich razem 100.");
+
+// Zadanie 2
+/* Napisz funkcję, która zsumuje przekazaną do niej tablicę i zwraca jej sumę.
+   Stwórz dowolną tablicę, a następnie przekaż ją do tej funkcji i wynik wypisz w konsoli.
+*/
