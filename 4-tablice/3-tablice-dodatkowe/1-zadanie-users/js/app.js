16bddd617c0fbd4270cf80226d8d4cf13ad5f28e
Peter <56226439+piotrax@users.noreply.github.com>
2020-09-21 21:23:53 +0200

    Update app.js

---
+++ 4-tablice/3-tablice-dodatkowe/1-zadanie-users/js/app.js
@@ -0,0 +1,9 @@
+// Zadanie 1
+// Wypisz imiona i nazwiska użytkowników oraz ich email np.
+// Claire Lucas -> clairelucas@memora.com
+for(let user of users){
+  console.log(`${user.name} -> ${user.email}`);
+}
+
+// Zadanie 2
+// Wypisz tablicę z wszystkimi użytkownikami pełnoletnimi
