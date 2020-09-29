b5a919f7ded70262970022fa3d61de057a7663fa
Peter <56226439+piotrax@users.noreply.github.com>
2020-09-29 13:36:26 +0200

    Update app.js

--- 4-tablice/3-tablice-dodatkowe/2-zadanie-cities/js/app.js
+++ 4-tablice/3-tablice-dodatkowe/2-zadanie-cities/js/app.js
@@ -1,6 +1,21 @@
 // Zadanie 1
 // Wypisz ile miast jest w Polsce
-console.log(cities.length);
+console.log(`ilość miast w Polsce: ${cities.length}`);
 
 // Zadanie 2
 // Wypisz ile jest ludzi w tych miastach
+citizens = 0;
+for(city of cities){
+  citizens += city.people;
+}
+console.log(`Ogółem liczba mieszkańców we wszystkich polskich miastach wynosi: ${citizens}`);
+
+// Zadanie 3
+// Wypisz pierwsze miasto, w którym jest ponad 10000 ludzi
+const findCity10k = cities.find(city => {
+  return city.people > 10000;
+})
+console.log(`Pierwsze znalezione miasto, które zamieszkuje ponad 10000 mieszkańców to ${findCity10k.name}`);
+
+// Zadanie 4
+// Wypisz miasta, w których ludzi jest ponad średnią
